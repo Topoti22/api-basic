913c9e6f28a376194536b3555aaa1f1eb66feefc
Topoti <bmtopoti@gmail.com>
2024-07-25 11:30:27 +0530

    database changes

--- Model/Database.php
+++ Model/Database.php
@@ -15,14 +15,57 @@
 			}
 		}
 		
-		protected function select($sql) {
-			if($result = $this->conn->query($sql)){
-				return array(true,$result);	
-			}else{
-				return array(false,$this->conn->error) ;
+		function build_bind_params($types, $values) {
+
+		// THEN I CREATE AN EMPTY ARRAY TO STORE THE FINAL OUTPUT
+			$bind_array = array();
+
+		// THEN I CREATE A TEMPORARY EMPTY ARRAY TO GROUP THE TYPES; THIS IS NECESSARY BECAUSE THE FINAL ARRAY ALL THE TYPES MUST BE A STRING WITH NO SPACES IN THE FIRST KEY OF THE ARRAY
+			
+			$i = array();
+			foreach ($types as $type) {
+				$i[] = $type;
 			}
+		// SO I IMPLODE THE TYPES ARRAY TO REMOVE COMMAS AND ADD IT AS KEY[0] IN THE BIND ARRAY
+			$bind_array[] = implode('', $i);
+
+		// FINALLY I LOOP THROUGH THE VALUES AND ADD THOSE AS SUBSEQUENT KEYS IN THE BIND ARRAY
+			foreach($values as $value) {
+				$bind_array[] = $value;
+			}
+			return $bind_array;
 		}
-	
+		
+		protected function select($sql,$types = [],$data = []) {
+			
+			if($stmt = $this->conn->prepare($sql)){
+				if(!empty($types)){
+					$params = $this->build_bind_params($types, $data);
+					
+					// PROPERLY SETUP THE PARAMS FOR BINDING WITH CALL_USER_FUNC_ARRAY
+					$tmp = array();
+					foreach ($params as $key => $value) $tmp[$key] = &$params[$key];
+
+					// PROPERLY BIND ARRAY TO THE STATEMENT
+					call_user_func_array(array($stmt , 'bind_param') , $tmp);
+				}
+				
+				//$stmt->bind_param($types, $data);
+				$stmt->execute() ;
+				$result = $stmt->get_result(); //gets mysqli result from a mysqli statement.
+											   // not possible to fetch the result set directly from 
+											   //mysqli statement.
+				
+				return array(true,$result);	
+				
+			}else{
+				return array(false, $this->conn->error);	
+			}
+			$stmt->close();
+			
+			
+		}
+			
 		public function __destruct() {
 			$this->conn = null;
 		}
--- Model/UserModel.php
+++ Model/UserModel.php
@@ -9,22 +9,34 @@ class UserModel extends Database
     public function getAllUsers($limit)
     {
 		
-		$sql = "select user_id,username,user_email,user_status from users order by user_id limit {$limit} " ;
-		$ret = $this->select($sql) ;
+		$types =[] ;
+		$data =[] ;
+		$types =["i"] ;
+		$data =[$limit] ;
+		$sql = "select user_id,username,user_email,user_status from users  order by user_id limit ? " ;
+		$ret = $this->select($sql,$types,$data) ;
+		//$ret = $this->select($sql) ;
 		
 		$jsonEncodedData = json_encode([]) ;
 		
 		$data = [] ;
 		if ($ret[0]){
 			if($ret[1]->num_rows > 0){
+				
 				$cnt = 0 ;
-				while($row = $ret[1]->fetch_array()){
-					//$dataRow = [] ;
-					//$r = 0 ;
-					//foreach($row as $key => $val){
-					//	$dataRow[$r] = $key => $val ;
-					//	$r++ ;
-					//}
+				/*
+				$rows = $ret[1]->fetch_all(MYSQLI_ASSOC) ;
+				foreach($rows as $row){
+					$dataRow = [] ;
+					foreach($row as $key => $val){
+						$dataRow[$key] =  $val ;
+						
+					}
+					$data[$cnt] = $dataRow ;
+					++$cnt ;
+				}*/
+					
+				while($row = $ret[1]->fetch_assoc()){
 					$data[$cnt] = array("id" =>  $row["user_id"],"name" =>  $row["username"],"email" =>  $row["user_email"],"status" => $row["user_status"]) ;
 					++$cnt ;
 				}
